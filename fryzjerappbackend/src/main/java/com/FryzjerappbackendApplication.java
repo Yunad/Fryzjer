66d0522ab1660d136003c9b8b447cdaf5d27d15b
Yunad <yunads@gmail.com>
2018-12-09 18:50:53 +0100

    fixed Spring Main class

--- fryzjerappbackend/src/main/java/com/FryzjerappbackendApplication.java
+++ fryzjerappbackend/src/main/java/com/FryzjerappbackendApplication.java
@@ -11,15 +11,15 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class FryzjerappbackendApplication implements ApplicationRunner {
-    private final UserRepository userRepository;
+    private final ClientRepository clientRepository;
     private final WorkerRepository workerRepository;
     private final ReservationRepository reservationRepository;
     private final ScheduleRepository scheduleRepository;
     private final ServiceRepository serviceRepository;
     private final RightRepository rightRepository;
 
-    public FryzjerappbackendApplication(UserRepository userRepository, WorkerRepository workerRepository, ReservationRepository reservationRepository, ScheduleRepository scheduleRepository, ServiceRepository serviceRepository, RightRepository rightRepository) {
-        this.userRepository = userRepository;
+    public FryzjerappbackendApplication(ClientRepository clientRepository, WorkerRepository workerRepository, ReservationRepository reservationRepository, ScheduleRepository scheduleRepository, ServiceRepository serviceRepository, RightRepository rightRepository) {
+        this.clientRepository = clientRepository;
         this.workerRepository = workerRepository;
         this.reservationRepository = reservationRepository;
         this.scheduleRepository = scheduleRepository;
@@ -33,25 +33,25 @@ public class FryzjerappbackendApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        final int firstSize = userRepository.findAll().size();
+        final int firstSize = clientRepository.findAll().size();
         System.out.println("firstSize = " + firstSize);
 
         final Client newClient = new Client("name1", "name2", "pass1", "email1");
 
-        final Client managedClient = userRepository.save(newClient); // managed by JPA
+        final Client managedClient = clientRepository.save(newClient); // managed by JPA
 
         System.out.println("managedClient = " + managedClient);
 
-        final int secondSize = userRepository.findAll().size();
+        final int secondSize = clientRepository.findAll().size();
         System.out.println("secondSize = " + secondSize);
 
-        final Optional<Client> foundClient = userRepository.findById(managedClient.getId());
+        final Optional<Client> foundClient = clientRepository.findById(managedClient.getId());
 
         System.out.println("foundClient = " + foundClient);
 
-        userRepository.deleteById(managedClient.getId());
+        clientRepository.deleteById(managedClient.getId());
 
-        final int thirdSize = userRepository.findAll().size();
+        final int thirdSize = clientRepository.findAll().size();
         System.out.println("thirdSize = " + thirdSize);
 
     }
